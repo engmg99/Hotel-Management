d534a9c040566f00bebba40326f3d34d4ec5c277
engmg99 <goyalmunish4306@gmail.com>
2023-12-08 17:41:25 +0530

    Initial Commit

---
+++ Hotel-Management-Service/src/test/java/com/lakeside/hotel/HotelManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakeside.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
